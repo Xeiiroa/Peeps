2d48a6e2b4c16de810c686763ec13d954bfc7fa0
Sintiria <Jaydentomlinson4@gmail.com>
2023-11-05 19:22:02 -0500

    Create database function

---
+++ database/__init__.py
@@ -0,0 +1,24 @@
+from os import path
+
+import sqlalchemy
+from sqlalchemy import create_engine #gbt added
+from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+
+from .models import Settings, Base
+
+load_dotenv()
+
+DB_NAME = 'DATABASE_NAME'
+engine = create_engine(f'sqlite:///{DB_NAME}', echo=True) 
+
+def create_database(): #!removed app, subject to return
+    #! still havent created a database but did import data 
+    if not path.exists("database/" + DB_NAME):
+        engine = create_engine(f'sqlite:///{DB_NAME}', echo=True)
+        Base.metadata.create_all(engine)
+        
+        with Session(engine) as session:
+            default_settings = Settings()
+            session.add(default_settings)
+            session.commit()
