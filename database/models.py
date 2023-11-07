78a518e83f818443548e1aca8aa7c779037366ce
Sintiria <Jaydentomlinson4@gmail.com>
2023-11-07 18:36:40 -0500

    Changed models
    Removed video capture device and send notifications in place of user phone number and record delay time

--- database/models.py
+++ database/models.py
@@ -23,7 +23,6 @@ class Settings(Base):
     #* created a id just to hold the primary key since the documentation says its needed can be removed if not actually needed
     id: Mapped[int] = mapped_column(primary_key=True)
     video_save_path: Mapped[str] = mapped_column(String, server_default=vidpath + "/static")
-    video_capture_device: Mapped[int] = mapped_column(nullable=True, server_default=None)#text("0") #*can bring findcam into the equation and get the default number, could also be a string naming the video device instead of an int
-    send_notifications: Mapped[bool] = mapped_column(default=False)
     phone_number : Mapped[int] = mapped_column(nullable=True, server_default=None)   
+    delay_time : Mapped[int] = mapped_column(default=120)   
     
