cb7df5c25b8b151b73a0ae428741d27e5d1e1579
Sintiria <Jaydentomlinson4@gmail.com>
2023-11-07 18:39:53 -0500

    added documentation and slight data changes
    added documentation to the init of the feed class as well as changing the delay time in the sleep function of get livefeed to the time in the database

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,32 +4,68 @@ from time import sleep
 import cv2
 import mediapipe as mp
 import datetime, time
+import sys
 from .notifications import send_alert
+from database.data import data_commands as Data
 
 class Feed():
+    """
+    All functions related to camera functionality 
+    """
     def __init__(self):
+        """
+        Parameters:
+                data: Calls the data functions class to let me grab data saved in my database
+                mp_face_detection: mediapipes face detection
+                mp_drawing: mediapipes box drawing (not in use just there if i choose to do something with it)
+                cam (int): webcam port number for opencv
+                detection (bool): checking if a face is detected while webcam is running
+                record_delay(int): the delay before livefeed has the ability to start recording (to try to combat instant recording so it doesnt alert if the person that turned the webcam is leaving view of the camera)
+                
+        """
+        
         #! may need self
+        self.data = Data()
         self.mp_face_detection = mp.solutions.face_detection(model_selection=1, min_detection_confidence=0.5)
         self.mp_drawing = mp.solutions.drawing_utils
         self.cam = self.find_cam()
         self.face_detection = self.mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5)
         self.detection = False
         self.get_livefeed()
+        self.record_delay= self.data.get_delay()
     
     # if a cam isnt found return false so that livefeed cant take place      
     def find_cam(self):
-        return 0
+        """
+        goes through a reasonable amount of ports in a computer and checks if any of them can produce a video frame, if no luck returns false so that get_livefeed can exit safely
+        
+        """
+        try:
+            for i in range (10):
+                cap = cv2.VideoCapture(i)
+                if cap.isOpened():
+                    ret, frame = cap.read()
+                    if frame is not None:
+                        return int(i) 
+            return False
+        except cv2.error as e:
+            logging.error(f"An error has occured when trying to get webcam: {str(e)}")
         
     
+    
     def get_livefeed(self):
+        """
+        Starts the recording processs and if a face is detected calls the start recording function
+        """
         self.detection=False
         
-        #*2 minute grace period to start recording
-        time.sleep(180) 
+        #set delay before webcam can start to record
+        time.sleep(self.data.get_delay()) 
         
         self.cap = cv2.VideoCapture(self.cam)
         if self.cam == False:
-            return
+            print("No webcam Found")
+            sys.exit()
         
         with self.mp_face_detection:
             while self.cap.isOpened():
@@ -38,6 +74,7 @@ class Feed():
                     print("empty frame")
                     continue
         
+                #saying not to draw boxes
                 image.flags.writeable=False #?
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 
