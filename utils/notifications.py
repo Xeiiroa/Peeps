afd1c062e77069e71042d5e8b09d68ce9c70444d
Sintiria <Jaydentomlinson4@gmail.com>
2023-11-19 14:57:16 -0500

    updated notifications.py
    moved from the provided default template to one that allows me to publish properly while mainting its ability to be dynamic

--- utils/notifications.py
+++ utils/notifications.py
@@ -2,28 +2,32 @@
 
 import os
 from dotenv import load_dotenv
-from twilio.rest import Client
-from database.data import data_commands as Data
+from twilio.rest import Client #type: ignore
+import twilio #type: ignore
+from database.data import data_commands as DATA
 
 
-load_dotenv()
-Data()
+load_dotenv('.env')
+Data = DATA()
 
-account_sid = 'SMS_ACCOUNT_SID'
-auth_token ='SMS_AUTH_TOKEN'
+account_sid = os.getenv('SMS_ACCOUNT_SID')
+auth_token = os.getenv('SMS_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
 def send_alert():
     recipient_number = Data.get_phone_number()
     if recipient_number == None:
-        print("number not found")
+        print("phone number not found")
         return
-    
-    message = client.messages \
-                .create(
-                     body="Motion has been detected on your webcam",
-                     from_='SMS_SENDER_NUMBER',
-                     to=recipient_number
-                 )
+    try:
+        message = client.messages \
+                    .create(
+                        body="Motion has been detected on your webcam",
+                        from_='SMS_SENDER_NUMBER',
+                        to=recipient_number
+                    )
+    except twilio.TwilioRestException as e:
+        print(e)
 
-
+if __name__ == "__main__":
+    send_alert()
