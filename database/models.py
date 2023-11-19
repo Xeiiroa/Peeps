bca59e92bf775080cc58bee82ea5dda26b95a0c3
Sintiria <Jaydentomlinson4@gmail.com>
2023-11-19 14:54:29 -0500

    updated models.py's default models
    cleaned up default models as well as the statement to get the videp savepath to get a consistent model regardless of where it is ran

--- database/models.py
+++ database/models.py
@@ -13,16 +13,13 @@ class Base(DeclarativeBase):
 
 
 class Settings(Base):
-    #todo Maybe : might have to insert a table that has all the data but might not have to
-    vidpath = str(os.path.dirname("Security-Camera"))
-    
-    #table wasnt created
+    default_vidpath = os.getcwd()
     
     __tablename__ = 'user_settings'
     
     #* created a id just to hold the primary key since the documentation says its needed can be removed if not actually needed
     id: Mapped[int] = mapped_column(primary_key=True)
-    video_save_path: Mapped[str] = mapped_column(String, server_default=vidpath + "/static")
-    phone_number : Mapped[int] = mapped_column(nullable=True, server_default=None)   
-    delay_time : Mapped[int] = mapped_column(default=120)   
+    video_save_path: Mapped[str] = mapped_column(String, server_default=default_vidpath + "/static")
+    phone_number : Mapped[str] = mapped_column(String, nullable=True, server_default=None)   
+    delay_time : Mapped[int] = mapped_column(server_default="120")   
     
